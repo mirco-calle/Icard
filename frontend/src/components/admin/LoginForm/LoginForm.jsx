a2b2db58b1402fa78fa16586a7529ea701baf303
mirco <mirco@gmail.com>
2025-08-22 17:19:52 -0400

    agregando validaciones

--- frontend/src/components/admin/LoginForm/LoginForm.jsx
+++ frontend/src/components/admin/LoginForm/LoginForm.jsx
@@ -1,11 +1,48 @@
 import "./LoginForm.scss";
 import { Button, Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 export const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => {
+      console.log(formValue);
+    },
+  });
+
   return (
-    <Form className="login-form-admin">
-      <Form.Input name="email" placeholder="Correo electrónico" />
-      <Form.Input name="password" placeholder="Contraseña" type="password" />
+    <Form className="login-form-admin" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="email"
+        placeholder="Correo electrónico"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name="password"
+        placeholder="Contraseña"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
       <Button type="submit" primary fluid content="Iniciar Sesion" />
     </Form>
   );
 };
+
+function initialValues() {
+  return {
+    email: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
+}
